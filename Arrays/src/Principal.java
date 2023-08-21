74bc1c17c0129ea81150b1cd83a30c23d50b0004
Nesrux <joaomarcosdevs@gmail.com>
2023-08-21 16:26:42 -0300

    aprendendo a iniciar arrays

---
+++ Arrays/src/Principal.java
@@ -0,0 +1,11 @@
+public class Principal {
+    public static void main(String[] args) {
+        //int o tipo [] declaração de array
+        //new int a instancia,
+        //[10] quantodade de elementos
+       //int[] notas = new int[10];
+
+       int [] notas = {8,7,94,5,6,5};
+
+    }
+}

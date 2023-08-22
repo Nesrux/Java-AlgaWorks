15e946355772cef3df94692a2b2c4ce3763f34f4
Nesrux <joaomarcosdevs@gmail.com>
2023-08-22 16:04:08 -0300

    aula 10

---
+++ Arrays/src/aula10/Principal.java
@@ -0,0 +1,16 @@
+package aula10;
+
+public class Principal {
+    public static void main(String[] args) {
+        //Array multiDimencional
+        String[][] cidades = new String[3][3];
+
+        cidades[0][0] = "uberlandia"; //linha 0 coluna 0
+        cidades[0][1] = "São paulo";
+        cidades[0][2] = "São caetano do sul";
+
+        cidades[1][0] = "Fortaleza";
+
+
+    }
+}

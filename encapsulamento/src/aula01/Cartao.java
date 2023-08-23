68e2e51199a75945b8bff1351a48b68b350fdf6e
Nesrux <joaomarcosdevs@gmail.com>
2023-08-23 19:47:32 -0300

    aula 04 (transformando a classe em um javabean)

--- encapsulamento/src/aula01/Cartao.java
+++ encapsulamento/src/aula01/Cartao.java
@@ -8,6 +8,7 @@ public class Cartao implements Serializable {
 
     private String titular;
     private double saldo;
+    private boolean ativo;
 
     public Cartao(String titular) {
         this.titular = titular;
@@ -17,12 +18,24 @@ public class Cartao implements Serializable {
         return titular;
     }
 
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
     public double getSaldo() {
         return saldo;
     }
 
-    public void setTitular(String titular) {
-        this.titular = titular;
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
     }
 
     public void debitar(double valorDebito) {

35dae9f623959799641595d4efb17bd67f24b685
Nesrux <joaomarcosdevs@gmail.com>
2023-08-25 19:37:09 -0300

    aula 08

--- encapsulamento/src/aula03/Principal.java
+++ encapsulamento/src/aula03/Principal.java
@@ -5,7 +5,6 @@ import org.w3c.dom.ls.LSOutput;
 public class Principal {
     public static void main(String[] args) {
         Horario horario  = new Horario(7,40);
-        horario.queHorasSao();
       //  System.out.println("Hora: " + horario.getHora() + " Minuto: " + horario.getMinuto());
     }
 

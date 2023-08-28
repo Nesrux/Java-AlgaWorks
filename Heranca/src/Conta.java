97cff6094e7f46176cf830da1f39523fc006d727
Nesrux <joaomarcosdevs@gmail.com>
2023-08-28 18:50:42 -0300

    Criando as classes do projeto

---
+++ Heranca/src/Conta.java
@@ -0,0 +1,52 @@
+public class Conta {
+    private Titular titular;
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+    public Titular getTitular() {
+        return titular;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setTitular(Titular titular) {
+        this.titular = titular;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public void sacar(double valorSaque) {
+        if (valorSaque <= 0) {
+            throw new IllegalArgumentException("Valor de saque não pode ser menor que 0");
+        }
+        if (saldo < valorSaque) {
+            throw new IllegalArgumentException("saldo insulficiente para realizar saque");
+        }
+        this.saldo -= valorSaque;
+    }
+
+    public void depositar(double valorDeposito) {
+        if (valorDeposito < 0) {
+            throw new IllegalArgumentException("Você nao pode depositar valores negativos");
+        }
+        saldo += valorDeposito;
+    }
+
+}

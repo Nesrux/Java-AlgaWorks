dbbcd9422ade493455651e9d50ddf3bbd67649f1
Nesrux <joaomarcosdevs@gmail.com>
2023-08-29 15:16:41 -0300

    aula 05

--- Heranca/src/Principal3.java
+++ Heranca/src/Principal3.java
@@ -1,4 +1,3 @@
-import com.nesrux.banco.Conta;
 import com.nesrux.banco.ContaEspecial;
 import com.nesrux.banco.Titular;
 
@@ -6,14 +5,20 @@ public class Principal3 {
     public static void main(String[] args) {
         ContaEspecial conta1 = new ContaEspecial();
         conta1.setTitular(new Titular("João", "123456789"));
-        conta1.depositar(100);
         conta1.setAgencia(1234);
         conta1.setNumero(99955533);
-        conta1.setLimiteChequeEspecial(1000);
         conta1.setTarifaChequeEspecial(90);
+        conta1.setLimiteChequeEspecial(99999);
 
-        conta1.sacar(200);
         conta1.imprimirDemostrativo();
-        conta1.debitarTatifaMensal();
+
+//        conta1.depositar(100);
+//        conta1.imprimirDemostrativo();
+//
+//        conta1.sacar(200);
+//        conta1.imprimirDemostrativo();
+//
+//        conta1.debitarTatifaMensal();
+//        conta1.imprimirDemostrativo();
     }
 }
--- Heranca/src/com/nesrux/banco/ContaEspecial.java
+++ Heranca/src/com/nesrux/banco/ContaEspecial.java
@@ -7,13 +7,12 @@ public class ContaEspecial extends ContaInvestimento {
     private double limiteChequeEspecial;
     private double tarifaChequeEspecial;
 
+    public double getLimiteChequeEspecial() {
+        return limiteChequeEspecial;
+    }
 
     public double getSaldoDisponivel() {
         return getSaldo() + getLimiteChequeEspecial();
-    }
-
-    public double getLimiteChequeEspecial() {
-        return limiteChequeEspecial;
     }
 
     public void setLimiteChequeEspecial(double limiteChequeEspecial) {
@@ -32,4 +31,24 @@ public class ContaEspecial extends ContaInvestimento {
         sacar(getTarifaChequeEspecial());
     }
 
+    @Override
+    public void sacar(double valorSaque) {
+        if (valorSaque <= 0) {
+            throw new IllegalArgumentException("Valor de saque não pode ser menor que 0");
+        }
+        if (getSaldoDisponivel() < valorSaque) {
+            throw new IllegalArgumentException("saldo insulficiente para realizar saque");
+        }
+        this.saldo -= valorSaque;
+    }
+
+    @Override
+    public void imprimirDemostrativo() {
+        System.out.println();
+        System.out.printf("Agencia de código: %d%n", getAgencia());
+        System.out.printf("com.nesrux.banco.Conta de código: %d%n", getNumero());
+        System.out.printf("com.nesrux.banco.Titular:  %s%n", getTitular().getNome());
+        System.out.printf("Saldo: %.2f%n", getSaldo());
+        System.out.printf("Saldo disponivel %.2f", getSaldoDisponivel());
+    }
 }

c3b53b8b192b2904b61f55eef033c1450b39927b
Nesrux <joaomarcosdevs@gmail.com>
2023-08-29 12:58:01 -0300

    aula 02

--- Heranca/src/Principal3.java
+++ Heranca/src/Principal3.java
@@ -1,17 +1,19 @@
 import com.nesrux.banco.Conta;
 import com.nesrux.banco.Titular;
 
-public class Principal {
+public class Principal3 {
     public static void main(String[] args) {
         Conta conta1 = new Conta();
         conta1.setTitular(new Titular("João", "123456789"));
-        conta1.depositar(1000);
+        conta1.depositar(100);
         conta1.setAgencia(1234);
         conta1.setNumero(99955533);
-        conta1.setTipo(Conta.NORMAL);
+        conta1.setTipo(Conta.ESPECIAL);
+        conta1.setLimiteChequeEspecial(1000);
+        conta1.setTarifaChequeEspecial(90);
 
-        conta1.sacar(25.50);
+        conta1.sacar(200);
         conta1.imprimirDemostrativo();
-        conta1.creditarRendimentos(0.10);
+        conta1.debitarTatifaMensal();
     }
 }
--- Heranca/src/com/nesrux/banco/Conta.java
+++ Heranca/src/com/nesrux/banco/Conta.java
@@ -1,10 +1,23 @@
 package com.nesrux.banco;
 
 public class Conta {
+    public static final int NORMAL = 0;
+    public static final int INVESTIMENTO = 1;
+    public static final int ESPECIAL = 2;
+
+
     private Titular titular;
     private int agencia;
     private int numero;
     private double saldo;
+    private int tipo = NORMAL;
+
+    //Conta investimento
+    private double valorTotalRendimentos;
+
+    //Conta Especial
+    private double limiteChequeEspecial;
+    private double tarifaChequeEspecial;
 
     public Titular getTitular() {
         return titular;
@@ -34,11 +47,62 @@ public class Conta {
         this.numero = numero;
     }
 
+    public int getTipo() {
+        return tipo;
+    }
+
+    public double getSaldoDisponivel() {
+        return getSaldo() + getLimiteChequeEspecial();
+    }
+
+    public void setTipo(int tipo) {
+        if (tipo != NORMAL && tipo != INVESTIMENTO && tipo != ESPECIAL) {
+            throw new IllegalArgumentException(String.format("Tipo inválido: %d", tipo));
+        }
+        if (getTipo() != ESPECIAL) {
+            this.limiteChequeEspecial = 0;
+        }
+        this.tipo = tipo;
+    }
+
+    public double getValorTotalRendimentos() {
+        return valorTotalRendimentos;
+    }
+
+    public void creditarRendimentos(double percentualJuros) {
+        if (getTipo() != NORMAL) {
+            double valorRendimentos = getSaldo() * percentualJuros / 100;
+            this.valorTotalRendimentos += valorRendimentos;
+            depositar(valorRendimentos);
+        } else {
+            throw new RuntimeException("Tipo de conta inválida");
+        }
+    }
+
+    public double getLimiteChequeEspecial() {
+        return limiteChequeEspecial;
+    }
+
+    public void setLimiteChequeEspecial(double limiteChequeEspecial) {
+        if (getTipo() != ESPECIAL) {
+            throw new RuntimeException("Esse tipo de conta nao permite limite de cheque Especial");
+        }
+        this.limiteChequeEspecial = limiteChequeEspecial;
+    }
+
+    public double getTarifaChequeEspecial() {
+        return tarifaChequeEspecial;
+    }
+
+    public void setTarifaChequeEspecial(double tarifaChequeEspecial) {
+        this.tarifaChequeEspecial = tarifaChequeEspecial;
+    }
+
     public void sacar(double valorSaque) {
         if (valorSaque <= 0) {
             throw new IllegalArgumentException("Valor de saque não pode ser menor que 0");
         }
-        if (saldo < valorSaque) {
+        if (getSaldoDisponivel() < valorSaque) {
             throw new IllegalArgumentException("saldo insulficiente para realizar saque");
         }
         this.saldo -= valorSaque;
@@ -51,12 +115,21 @@ public class Conta {
         saldo += valorDeposito;
     }
 
-    public void imprimirDemostrativo(){
+    public void debitarTatifaMensal() {
+        if (getTipo() == ESPECIAL) {
+            sacar(getTarifaChequeEspecial());
+        } else {
+            throw new RuntimeException("Não pode debitar tarifa especial nesse  tipo de conta");
+        }
+    }
+
+
+    public void imprimirDemostrativo() {
         System.out.println();
-        System.out.printf("Agencia de código: %d%n",getAgencia());
-        System.out.printf("com.nesrux.banco.Conta de código: %d%n",getNumero());
-        System.out.printf("com.nesrux.banco.Titular:  %s%n",getTitular().getNome());
-        System.out.printf("Saldo: %.2f%n",getSaldo());
+        System.out.printf("Agencia de código: %d%n", getAgencia());
+        System.out.printf("com.nesrux.banco.Conta de código: %d%n", getNumero());
+        System.out.printf("com.nesrux.banco.Titular:  %s%n", getTitular().getNome());
+        System.out.printf("Saldo: %.2f%n", getSaldo());
     }
 
 }

f5cbf7b3f2d889e1f759a7e036dd36c4b37277c3
Nesrux <joaomarcosdevs@gmail.com>
2023-08-30 13:07:57 -0300

    aula 14

--- Heranca/src/com/nesrux/banco/ContaEspecial.java
+++ Heranca/src/com/nesrux/banco/ContaEspecial.java
@@ -1,50 +1,63 @@
 package com.nesrux.banco;
 
 public class ContaEspecial extends ContaInvestimento {
+    private double tarifaMensal;
     private double limiteChequeEspecial;
-    private double tarifaEspecial;
 
-     public ContaEspecial(Titular titular, int agencia, int numero, double tarifaEspecial) {
+    public ContaEspecial(Titular titular, int agencia, int numero, double tarifaMensal) {
         super(titular, agencia, numero);
-        this.tarifaEspecial = tarifaEspecial;
+        this.tarifaMensal = tarifaMensal;
+    }
+
+    public double getTarifaMensal() {
+        return tarifaMensal;
+    }
+
+    public void setTarifaMensal(double tarifaMensal) {
+        this.tarifaMensal = tarifaMensal;
     }
 
     public double getLimiteChequeEspecial() {
         return limiteChequeEspecial;
     }
 
-    public double getSaldoDisponivel() {
-        return getSaldo() + getLimiteChequeEspecial();
-    }
-
     public void setLimiteChequeEspecial(double limiteChequeEspecial) {
         this.limiteChequeEspecial = limiteChequeEspecial;
     }
 
-    public double getTarifaChequeEspecial() {
-        return tarifaEspecial;
-    }
-
-    public void setTarifaChequeEspecial(double tarifaChequeEspecial) {
-        this.tarifaEspecial = tarifaChequeEspecial;
-    }
-
-    public void debitarTatifaMensal() {
-        sacar(getTarifaChequeEspecial());
+    public double getSaldoDisponivel() {
+        return getSaldo() + getLimiteChequeEspecial();
     }
 
     @Override
-    protected void validarSaque(double valorSaque) {
+    public final void imprimirDemonstrativo() {
+        super.imprimirDemonstrativo();
+        System.out.printf("Saldo disponível: %.2f%n", getSaldoDisponivel());
+    }
+
+    @Override
+    protected final void validarSaldoParaSaque(double valorSaque) {
         if (getSaldoDisponivel() < valorSaque) {
-            throw new IllegalArgumentException("saldo insulficiente para realizar saque");
+            throw new RuntimeException("Saldo insuficiente para saque");
         }
     }
 
-    @Override
-    public void imprimirDemostrativo() {
-        super.imprimirDemostrativo();
-        System.out.printf("Saldo disponivel %.2f", getSaldoDisponivel());
+    public void debitarTarifaMensal() {
+        sacar(getTarifaMensal());
     }
+
+    @Override
+    public String toString() {
+        return "ContaEspecial{" +
+                "titular=" + getTitular() +
+                ", agencia=" + getAgencia() +
+                ", numero=" + getNumero() +
+                ", valorTotalRendimentos=" + getValorTotalRendimentos() +
+                ", tarifaMensal=" + tarifaMensal +
+                ", limiteChequeEspecial=" + limiteChequeEspecial +
+                '}';
+    }
+
 
 
 }

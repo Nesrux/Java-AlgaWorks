d9b62e898ebe0c828d26a4bd57e878070d8150a8
Nesrux <joaomarcosdevs@gmail.com>
2023-08-30 14:05:15 -0300

    Desafio heranca

--- Desafio_Heranca/src/desafio/domain/model/Horelite.java
+++ Desafio_Heranca/src/desafio/domain/model/Horelite.java
@@ -1,5 +1,9 @@
 package desafio.domain.model;
 
 public record Horelite (String nome, String anoMes, double valorSalario){
-    public void imprimir(){}
+    public void imprimir(){
+        System.out.println("Nome: " + nome);
+        System.out.println("Data de admição: " + anoMes);
+        System.out.println("Salario: " + valorSalario);
+    }
 }

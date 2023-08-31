08f95f211ef639b8007bf7f60027590efdac25eb
Nesrux <joaomarcosdevs@gmail.com>
2023-08-31 14:48:46 -0300

    Polimorfismo aula 03

--- Polimorfismo/src/main3.java
+++ Polimorfismo/src/main3.java
@@ -1,7 +1,4 @@
-import com.banco.CaixaEletronico;
-import com.banco.Conta;
-import com.banco.ContaEspecial;
-import com.banco.Titular;
+import com.banco.*;
 
 public class main3 {
     public static void main(String[] args) {
@@ -10,16 +7,22 @@ public class main3 {
 
         Titular titular1 = new Titular("Marcelo Almeida", "123456");
         Titular titular2 = new Titular("Thiago Malaquias", "654321");
+        Titular titular3 = new Titular("Gerundina almeida", "987654");
 
         ContaEspecial conta1 = new ContaEspecial(titular1, 999,99, 50);
+        conta1.setLimiteChequeEspecial(500);
 
-        ContaEspecial conta2 = new ContaEspecial(titular1, 568, 878, 100);
+
+
+        ContaEspecial conta2 = new ContaEspecial(titular2, 568, 878, 100);
+
+        ContaSalario conta3 = new ContaSalario(titular3, 456, 784, 2500);
 
         conta1.depositar(300);
 
-        caixaEletronico.transferir(conta1, conta2, 50);
+        caixaEletronico.transferir(conta1, conta2, 400);
 
         conta1.imprimirDemonstrativo();
-        conta2.imprimirDemonstrativo();
+        conta3.imprimirDemonstrativo();
     }
 }

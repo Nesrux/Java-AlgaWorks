3b641de358f0592cc0aeefd83b8b02877422b9f7
Nesrux <joaomarcosdevs@gmail.com>
2023-08-31 13:18:37 -0300

    Desafio heranca completo

--- Desafio_Heranca/src/desafio/domain/model/Funcionario.java
+++ Desafio_Heranca/src/desafio/domain/model/Funcionario.java
@@ -1,5 +1,7 @@
 package desafio.domain.model;
 
+import java.util.Objects;
+
 public class Funcionario {
     private String nome;
     private double valorHora;
@@ -25,15 +27,29 @@ public class Funcionario {
         this.valorHora = valorHora;
     }
 
-    protected double calcularSalario(int horasTrabalhadas){
+    protected double calcularSalario(int horasTrabalhadas) {
         return getValorHora() * horasTrabalhadas;
     }
-    public Horelite gerarOrelite(int horasTrabalhadas, String mesAno){
+
+    public Horelite gerarOrelite(int horasTrabalhadas, String mesAno) {
         return new Horelite(getNome(), mesAno, calcularSalario(horasTrabalhadas));
     }
 
     @Override
     public String toString() {
         return String.format("Funcionário: %s, valorHora : %.2f", getNome(), getValorHora());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Funcionario that = (Funcionario) o;
+        return Objects.equals(nome, that.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome);
     }
 }

cd9768dc8acd8e4dcbbdfc99de8092badeae2015
Nesrux <joaomarcosdevs@gmail.com>
2023-08-31 14:01:40 -0300

    Polimorfismo aula 01

--- Polimorfismo/src/main2.java
+++ Polimorfismo/src/main2.java
@@ -5,6 +5,9 @@ import com.banco.Titular;
 public class main2 {
     public static void main(String[] args) {
         Titular titular = new Titular("Jaime", "123456");
+       /*Nesse caso, como ContaSalario herda diretamente de Conta e nao de seu subtipos, como Conta
+       * Especial e conta investimento, nao pode ser feito o cast direto para ambos os casos,
+       * somente para o super tipo CONTA*/
         Conta conta = new ContaSalario(titular, 456,564, 18000);
 
 

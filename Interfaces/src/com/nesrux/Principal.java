5fb271027543ad32501483277ee1f1722f5092bf
Nesrux <joaomarcosdevs@gmail.com>
2023-09-03 18:20:27 -0300

    Aula de interfaces 02

--- Interfaces/src/com/nesrux/Principal.java
+++ Interfaces/src/com/nesrux/Principal.java
@@ -1,8 +1,16 @@
 package com.nesrux;
 
+import com.nesrux.contaspagar.service.ServicoContaPagar;
+import com.nesrux.pagamento.DocumentoPagavel;
+
 public class Principal {
     public static void main(String[] args) {
+        ServicoContaPagar servicoContaPagar = new ServicoContaPagar();
 
+        //TODO precisamos instanciar uma classe documentoPagavel
+        DocumentoPagavel documento = null;
+
+        servicoContaPagar.pagar(documento);
 
     }
 }

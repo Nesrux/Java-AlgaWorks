1c2684a9d516da2f129282d75b7f139e552f159f
Nesrux <joaomarcosdevs@gmail.com>
2023-09-03 17:51:46 -0300

    Aula de interfaces 01

--- Interfaces/src/com/nesrux/contaspagar/service/ServicoContaPagar.java
+++ Interfaces/src/com/nesrux/contaspagar/service/ServicoContaPagar.java
@@ -1,10 +1,17 @@
 package com.nesrux.contaspagar.service;
 
+import com.nesrux.pagamento.Beneficiario;
 import com.nesrux.pagamento.DocumentoPagavel;
 
 public class ServicoContaPagar {
 
-    public void pagar(DocumentoPagavel documento){
+    public void pagar(DocumentoPagavel documento) {
+        Beneficiario beneficiario = documento.getBeneficiario();
+
+        if (beneficiario.naoPossuiChavePix()) {
+            throw new RuntimeException("Beneficiario não possui chave Pix");
+        }
+        System.out.printf("DEBUG: efetuando PIX para %s, no valor de %.2f com a chave pix %s", beneficiario.getNome(), documento.getValorTotal(), beneficiario.getChavePix());
 
     }
 }

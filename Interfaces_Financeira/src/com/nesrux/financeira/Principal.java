41303a7ae5034bc010068dd24c89c19811ce0f41
Nesrux <joaomarcosdevs@gmail.com>
2023-09-04 18:43:15 -0300

    Aula de interfaces 05 (Projeto financeira)

--- Interfaces_Financeira/src/com/nesrux/financeira/Principal.java
+++ Interfaces_Financeira/src/com/nesrux/financeira/Principal.java
@@ -1,4 +1,15 @@
 package com.nesrux.financeira;
 
+import com.nesrux.financeira.modelo.Fazenda;
+import com.nesrux.financeira.modelo.Industria;
+import com.nesrux.financeira.servico.ServicoFinanciamento;
+
 public class Principal {
+    public static void main(String[] args) {
+        var servicoFinanciamento = new ServicoFinanciamento();
+        var fazenda = new Fazenda();
+        var industria = new Industria();
+
+        servicoFinanciamento.solicitarFinanciamento(fazenda, 600_000);
+    }
 }
--- Interfaces_Financeira/src/com/nesrux/financeira/servico/ServicoFinanciamento.java
+++ Interfaces_Financeira/src/com/nesrux/financeira/servico/ServicoFinanciamento.java
@@ -1,4 +1,21 @@
 package com.nesrux.financeira.servico;
 
+import com.nesrux.financeira.modelo.Empresa;
+
 public class ServicoFinanciamento {
+
+    public void solicitarFinanciamento(Empresa empresa, double valorSolicitado) {
+        double limiteAprovado = empresa.getLimiteAprovado();
+
+        if (limiteAprovado < valorSolicitado) {
+            throw new RuntimeException(String.format("Financiamente não aprovado. Limite de %.2f", limiteAprovado));
+        }
+
+        System.out.printf("DEBUG: Financiamento aprovado. limite máximo de %.2f%n", limiteAprovado);
+
+    }
+
+    public double consultarLimiteAProvado(Empresa empresa) {
+        return 2.0;
+    }
 }

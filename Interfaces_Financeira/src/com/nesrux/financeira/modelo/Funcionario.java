a8af601d512274cee5bdad2137cd1fc2172cc1f9
Nesrux <joaomarcosdevs@gmail.com>
2023-09-05 15:39:11 -0300

    Aula de interfaces 07

---
+++ Interfaces_Financeira/src/com/nesrux/financeira/modelo/Funcionario.java
@@ -0,0 +1,36 @@
+package com.nesrux.financeira.modelo;
+
+import com.nesrux.financeira.repository.ClienteFinanciavel;
+
+public class Funcionario implements ClienteFinanciavel {
+    public static final int LIMITE_CREDITO_SALARIO = 5;
+
+    private String nome;
+    private double salarioMensal;
+
+    public Funcionario(String nome, double salarioMensal) {
+        this.nome = nome;
+        this.salarioMensal = salarioMensal;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSalarioMensal() {
+        return salarioMensal;
+    }
+
+    public void setSalarioMensal(double salarioMensal) {
+        this.salarioMensal = salarioMensal;
+    }
+
+    @Override
+    public double calcularLimiteAprovado() {
+        return getSalarioMensal() * LIMITE_CREDITO_SALARIO;
+    }
+}

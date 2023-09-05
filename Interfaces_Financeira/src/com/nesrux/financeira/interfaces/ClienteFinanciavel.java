10c1d911092a7e4a1f1d8e81d84bceaa41910b78
Nesrux <joaomarcosdevs@gmail.com>
2023-09-05 19:01:23 -0300

    Aula de interfaces 08

--- Interfaces_Financeira/src/com/nesrux/financeira/interfaces/ClienteFinanciavel.java
+++ Interfaces_Financeira/src/com/nesrux/financeira/interfaces/ClienteFinanciavel.java
@@ -4,7 +4,13 @@ public interface ClienteFinanciavel {
 
      double calcularLimiteAprovado();
 
-    default double calcularTaxaDeJuros(double taxaDeJuros){
-         return 0;
-    };
+    default double calcularTaxaDeJuros(double valorSolicitado){
+       if(valorSolicitado <= 100_000){
+           return 1.0;
+       } else if (valorSolicitado <= 1_000_000) {
+           return 1.5;
+       }
+
+        return 2.0;
+    }
 }

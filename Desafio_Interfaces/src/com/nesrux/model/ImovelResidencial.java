7e8738034c889246f8d6620e0f149613dafe138f
Nesrux <joaomarcosdevs@gmail.com>
2023-09-07 19:42:32 -0300

    Desafio interfaces

--- Desafio_Interfaces/src/com/nesrux/model/ImovelResidencial.java
+++ Desafio_Interfaces/src/com/nesrux/model/ImovelResidencial.java
@@ -1,4 +1,39 @@
 package com.nesrux.model;
 
-public class ImovelResidencial {
+import com.nesrux.interfaces.BemSeguravel;
+
+public class ImovelResidencial  implements BemSeguravel {
+    private double valorMercado;
+    private int areaConstruida;
+
+    public ImovelResidencial(double valorMercado, int areaConstruida) {
+        this.valorMercado = valorMercado;
+        this.areaConstruida = areaConstruida;
+    }
+
+    @Override
+    public double calcularValorPremio() {
+        return 0;
+    }
+
+    @Override
+    public String descrever() {
+        return null;
+    }
+
+    public double getValorMercado() {
+        return valorMercado;
+    }
+
+    public void setValorMercado(double valorMercado) {
+        this.valorMercado = valorMercado;
+    }
+
+    public int getAreaConstruida() {
+        return areaConstruida;
+    }
+
+    public void setAreaConstruida(int areaConstruida) {
+        this.areaConstruida = areaConstruida;
+    }
 }

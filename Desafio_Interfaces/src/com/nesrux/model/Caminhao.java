c2f0bb239822337a663d7ebf847809cfd2fd93e4
Nesrux <joaomarcosdevs@gmail.com>
2023-09-07 19:40:18 -0300

    Desafio interfaces

--- Desafio_Interfaces/src/com/nesrux/model/Caminhao.java
+++ Desafio_Interfaces/src/com/nesrux/model/Caminhao.java
@@ -1,4 +1,28 @@
 package com.nesrux.model;
 
-public class Caminhao {
+public class Caminhao extends VeiculoAutomotor {
+    private int quantidadeEixos;
+
+    public Caminhao(String modelo, double valorMercado, int anoFabricacao, int quantidadeEixos) {
+        super(modelo, valorMercado, anoFabricacao);
+        this.quantidadeEixos = quantidadeEixos;
+    }
+
+    @Override
+    public double calcularValorPremio() {
+        return 0;
+    }
+
+    @Override
+    public String descrever() {
+        return null;
+    }
+
+    public int getQuantidadeEixos() {
+        return quantidadeEixos;
+    }
+
+    public void setQuantidadeEixos(int quantidadeEixos) {
+        this.quantidadeEixos = quantidadeEixos;
+    }
 }
--- Desafio_Interfaces/src/com/nesrux/model/CarroParticular.java
+++ Desafio_Interfaces/src/com/nesrux/model/CarroParticular.java
@@ -1,4 +1,18 @@
 package com.nesrux.model;
 
-public class CarroParticular {
+public class CarroParticular extends VeiculoAutomotor {
+    public CarroParticular(String modelo, double valorMercado, int anoFabricacao) {
+        super(modelo, valorMercado, anoFabricacao);
+    }
+
+
+    @Override
+    public double calcularValorPremio() {
+        return 0;
+    }
+
+    @Override
+    public String descrever() {
+        return null;
+    }
 }

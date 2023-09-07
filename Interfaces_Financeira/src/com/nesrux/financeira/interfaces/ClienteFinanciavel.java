de5c4a768ef78ee9b10cc460b24e31009418a624
Nesrux <joaomarcosdevs@gmail.com>
2023-09-07 17:06:00 -0300

    Aula de interfaces 11-12

--- Interfaces_Financeira/src/com/nesrux/financeira/interfaces/ClienteFinanciavel.java
+++ Interfaces_Financeira/src/com/nesrux/financeira/interfaces/ClienteFinanciavel.java
@@ -1,21 +1,23 @@
 package com.nesrux.financeira.interfaces;
 
 public interface ClienteFinanciavel {
+    double JUROS_BAIXO_RISCO = 1.0;
+    double JUROS_MEDIO_RISCO = 1.5;
+    double JUROS_ALTO_RISCO = 2.0;
     //Automaticamente o compilador vai transformar esse int numero teste em
     //Public static final int numeroTeste = 0
     //é a mesma ideia de nao colocar abstract em interfaces
 
-    int numeroTeste = 12;
 
     double calcularLimiteAprovado();
 
     default double calcularJuros(double valorSolicitado) {
         if (isEmpresaDePequenoValor(valorSolicitado)) {
-            return 1.0;
+            return JUROS_BAIXO_RISCO;
         } else if (IsEmpresaDeMedioValor(valorSolicitado)) {
-            return 1.5;
+            return JUROS_MEDIO_RISCO;
         }
-        return 2.0;
+        return JUROS_ALTO_RISCO;
     }
 
     private static boolean IsEmpresaDeMedioValor(double valorSolicitado) {

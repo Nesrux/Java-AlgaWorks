0ba618ac8ae48ec81b1dc65f9c27ce3ddb46c074
Nesrux <joaomarcosdevs@gmail.com>
2023-09-08 16:29:36 -0300

    Boas praticas com interfaces aula 02

--- BoasPraticas_interfaces/src/Principal.java
+++ BoasPraticas_interfaces/src/Principal.java
@@ -1,13 +1,10 @@
-import com.nesrux.locadora.Locacao;
-import com.nesrux.locadora.Notebook;
-import com.nesrux.locadora.Precificacao;
-import com.nesrux.locadora.PrecificacaoPorHora;
+import com.nesrux.locadora.*;
 
 public class Principal {
     public static void main(String[] args) {
 
         Notebook notebook = new Notebook("Idea Pad ryzen 5 8gb ram", 5, 100);
-        Precificacao precificacao = new PrecificacaoPorHora();
+        Precificacao precificacao = new PrecificacaoPorDia();
 
         Locacao locacao = new Locacao(notebook, precificacao);
         System.out.println(locacao.calcularValorDevido(10));
--- BoasPraticas_interfaces/src/com/nesrux/locadora/PrecificacaoPorDia.java
+++ BoasPraticas_interfaces/src/com/nesrux/locadora/PrecificacaoPorDia.java
@@ -3,6 +3,7 @@ package com.nesrux.locadora;
 public class PrecificacaoPorDia implements Precificacao {
     @Override
     public double calcularValorTotal(Notebook notebook, int horasUtilizadas) {
-        return 0;
+        double dias = Math.ceil(horasUtilizadas / 24d);
+        return notebook.getPrecoDia() * dias;
     }
 }

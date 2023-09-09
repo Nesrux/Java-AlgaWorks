dce09f0e89133769ed2a0b245ddf1a79670e7a67
Nesrux <joaomarcosdevs@gmail.com>
2023-09-09 12:52:44 -0300

    Exceptions aula 02

---
+++ Exceptions/src/com/nesrux/estoque/Produto.java
@@ -0,0 +1,35 @@
+package com.nesrux.estoque;
+
+public class Produto {
+    private String nome;
+    private int quantidadeEstoque;
+    private boolean ativo;
+
+    public Produto(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    public void setQuantidadeEstoque(int quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+}

66b42663fb7e46b87bb05168b69b7ccf03a3e4c8
Nesrux <joaomarcosdevs@gmail.com>
2023-09-09 13:22:03 -0300

    Exceptions aula 02

--- Exceptions/src/com/nesrux/Principal.java
+++ Exceptions/src/com/nesrux/Principal.java
@@ -4,8 +4,10 @@ import com.nesrux.estoque.Produto;
 
 public class Principal {
     public static void main(String[] args) {
-        Produto produto = new Produto("Motorola moto G 10");
+        Produto produto = new Produto(null);
         produto.setQuantidadeEstoque(10);
+        produto.ativar();
+        produto.removerDoEstoque(10);
 
         System.out.println("Estoque: " + produto.getQuantidadeEstoque());
     }
--- Exceptions/src/com/nesrux/estoque/Produto.java
+++ Exceptions/src/com/nesrux/estoque/Produto.java
@@ -6,7 +6,9 @@ public class Produto {
     private boolean ativo;
 
     public Produto(String nome) {
-        this.nome = nome;
+        setNome(nome);
+
+
     }
 
     public String getNome() {
@@ -14,6 +16,9 @@ public class Produto {
     }
 
     public void setNome(String nome) {
+        if (nome == null) {
+            throw new NullPointerException("O nome do produto não pode ser nulo ");
+        }
         this.nome = nome;
     }
 
@@ -29,7 +34,29 @@ public class Produto {
         return ativo;
     }
 
-    public void setAtivo(boolean ativo) {
-        this.ativo = ativo;
+    public boolean isInanivo() {
+        return !isAtivo();
+    }
+
+    public void ativar() {
+        this.ativo = true;
+    }
+
+    public void desativar() {
+        this.ativo = false;
+    }
+
+    public void removerDoEstoque(int quantidade) {
+        if (quantidade < 0) {
+            throw new IllegalArgumentException("O argumento quantidade não pode ser negativo!");
+        }
+        if (isInanivo()) {
+            throw new IllegalStateException("Não é possivel retitar o produto do estoque, pois ele esta inativo!");
+        }
+        if (getQuantidadeEstoque() - quantidade < 0) {
+            throw new IllegalArgumentException("Não é possivel realizar esta operação," +
+                    " pois o estoque deste produto ficara negativo");
+        }
+        setQuantidadeEstoque(getQuantidadeEstoque() - quantidade);
     }
 }

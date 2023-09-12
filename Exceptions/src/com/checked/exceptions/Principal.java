e487ef1b10313093ad692bbec2db10db69930fe3
Nesrux <joaomarcosdevs@gmail.com>
2023-09-12 13:26:46 -0300

    Exceptions aula 08

--- Exceptions/src/com/checked/exceptions/Principal.java
+++ Exceptions/src/com/checked/exceptions/Principal.java
@@ -1,4 +1,13 @@
 package com.checked.exceptions;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Principal {
+    public static void main(String[] args) throws IOException {
+        Path caminho = Path.of("C:\\Users\\jucaj\\Downloads\\teste.txt");
+
+        Files.createFile(caminho);
+    }
 }

c9443e00969648575606195dbc0fdcfec973e222
Nesrux <joaomarcosdevs@gmail.com>
2023-09-15 19:35:46 -0300

    generics aula 03

--- generics/src/com/introducao/generics/Principal3.java
+++ generics/src/com/introducao/generics/Principal3.java
@@ -1,4 +1,19 @@
 package com.introducao.generics;
 
+import com.introducao.generics.crm.Cliente;
+import com.introducao.generics.crm.Funcionario;
+
 public class Principal3 {
+    public static void main(String[] args) {
+        Funcionario[] funcionarios = {
+                new Funcionario("cleiton", 45),
+                new Funcionario("Thainá", 22),
+                new Funcionario("marinalva", 39),
+        };
+        Cliente[] clientes = {
+                new Cliente("posto limamei", 25_000),
+                new Cliente("mecanica simas turbo", 35_000),
+                new Cliente("two pay pal", 785_000)
+        };
+    }
 }

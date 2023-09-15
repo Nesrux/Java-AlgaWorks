a2d7913abd9c4d0076855f7811a1e27a17696a2a
Nesrux <joaomarcosdevs@gmail.com>
2023-09-15 19:53:33 -0300

    generics aula 03

--- generics/src/com/introducao/generics/crm/Pesquisador.java
+++ generics/src/com/introducao/generics/crm/Pesquisador.java
@@ -1,10 +1,12 @@
 package com.introducao.generics.crm;
 
 public class Pesquisador {
-
-    public static <T> T obterPorNome(T[] entidades, String nome) {
+    // isso é lower bound and upper bound
+    public static <T extends Nomeavel> T obterPorNome(T[] entidades, String nome) {
         for (T entidade : entidades) {
-
+            if (entidade.getNome().equals(nome)) {
+                return entidade;
+            }
         }
         throw new RuntimeException("Funcionario nao encontrado!");
 

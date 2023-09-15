185c8a6e8eb8bb553123510e779c68e6dec1f47d
Nesrux <joaomarcosdevs@gmail.com>
2023-09-15 15:11:13 -0300

    generics aula 01

--- generics/src/com/introducao/generics/Principal.java
+++ generics/src/com/introducao/generics/Principal.java
@@ -7,17 +7,17 @@ import java.util.ArrayList;
 public class Principal {
     public static void main(String[] args) {
 
-        ArrayList clientest = new ArrayList();
+        ArrayList<Cliente> clientest = new ArrayList<>();
 
         clientest.add(new Cliente("joao", 1800));
         clientest.add(new Cliente("Maria", 2500));
 
         double totalFaturamento = 0d;
-        for (Object objeto : clientest) {
-            Cliente cliente = (Cliente) objeto;
-            totalFaturamento += cliente.getFaturamentoMensal();
 
+        for (Cliente cliente : clientest) {
+            totalFaturamento += cliente.getFaturamentoMensal();
         }
+
         System.out.println(totalFaturamento);
     }
 }

f25312c65d30511c949c38ea332b9dde0bc2d833
Nesrux <joaomarcosdevs@gmail.com>
2023-10-05 17:49:05 -0300

    Collections api

--- collections_Megasena/src/Principal.java
+++ collections_Megasena/src/Principal.java
@@ -1,12 +1,13 @@
-import java.util.Iterator;
-
 public class Principal {
     public static void main(String[] args) {
         Sorteador sorteador = new Sorteador();
-
-        Iterator<Integer> iteratorSort = sorteador.iterator();
-        while (iteratorSort.hasNext()) {
-            System.out.println(iteratorSort.next());
+        for (Integer i : sorteador) {
+            System.out.println((i));
         }
+
+//        Iterator<Integer> iteratorSort = sorteador.iterator();
+//        while (iteratorSort.hasNext()) {
+//            System.out.println(iteratorSort.next());
+//        }
     }
 }

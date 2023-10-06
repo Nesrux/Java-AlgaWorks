2c8efde57a2e9b2e0a6cc52700d512711518b396
Nesrux <joaomarcosdevs@gmail.com>
2023-10-06 20:29:44 -0300

    Enumeracoes

--- Enumeracoes/src/Pedido.java
+++ Enumeracoes/src/Pedido.java
@@ -3,14 +3,13 @@ public class Pedido {
     private String nomeCliente;
     private StatusPedido status = StatusPedido.RASCUNHO;
     private Origem origem = Origem.BALCAO;
+    private Double valor;
 
     public StatusPedido getStatus() {
         return status;
     }
 
-    public void setStatus(StatusPedido status) {
-        this.status = status;
-    }
+    //public void setStatus(StatusPedido status) {this.status = status;}
 
     public String getNomeCliente() {
         return nomeCliente;
@@ -20,6 +19,9 @@ public class Pedido {
         this.nomeCliente = nomeCliente;
     }
 
+    public Origem getOrigem() {
+        return origem;
+    }
 
     public void setOrigem(Origem origem) {
         this.origem = origem;
@@ -27,6 +29,14 @@ public class Pedido {
 
     public int getTempoEntregueEmHoras() {
         return status.getTempoEmHoras();
+    }
+
+    public void cancelar() {
+        if (StatusPedido.RASCUNHO.equals(getStatus()) ||
+                StatusPedido.EMITIDO.equals(getStatus()) && this.valor < 100) {
+            this.status = StatusPedido.CANCELADO;
+        }
+        throw new IllegalStateException("Pedido Não pode ser cancelado");
     }
 
 }

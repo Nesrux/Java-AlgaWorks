7b1c11db3b3674a99bdb4e88b32f2589210be35b
Nesrux <joaomarcosdevs@gmail.com>
2023-10-06 13:02:23 -0300

    Enumeracoes

--- Enumeracoes/src/Pedido.java
+++ Enumeracoes/src/Pedido.java
@@ -1,18 +1,16 @@
 public class Pedido {
-    public static final int STATUS_RASCUNHO = 0;
-    public static final int STATUS_EMITIDO = 1;
-    public static final int STATUS_FATURADO = 2;
-    public static final int STATUS_DESPACHADO = 3;
-    public static final int STATUS_CANCELADO = 4;
-
-    public static final int ORIGEM_BALCAO = 100;
-    public static final int ORIGEM_ONLINE = 101;
 
     private String nomeCliente;
-    private int status = STATUS_RASCUNHO;
-    private int origem = ORIGEM_BALCAO;
+    private StatusPedido status = StatusPedido.RASCUNHO;
+    private Origem origem = Origem.BALCAO;
 
+    public StatusPedido getStatus() {
+        return status;
+    }
 
+    public void setStatus(StatusPedido status) {
+        this.status = status;
+    }
 
     public String getNomeCliente() {
         return nomeCliente;
@@ -22,36 +20,9 @@ public class Pedido {
         this.nomeCliente = nomeCliente;
     }
 
-    public int getStatus() {
-        return status;
-    }
 
-    public void setStatus(int status) {
-        if (status < STATUS_RASCUNHO || status > STATUS_CANCELADO) {
-            throw new IllegalArgumentException("codigo do status inválido");
-        }
-        this.status = status;
-    }
-
-    public int getOrigem() {
-        return origem;
-    }
-
-    public void setOrigem(int origem) {
-        if (origem < ORIGEM_BALCAO || origem > ORIGEM_ONLINE) {
-            throw new IllegalArgumentException("codigo do status inválido");
-        }
+    public void setOrigem(Origem origem) {
         this.origem = origem;
     }
 
-    public static String getDescricaoStatus(int status) {
-        return switch (status) {
-            case STATUS_RASCUNHO -> "Reascunho";
-            case STATUS_FATURADO -> "Faturado";
-            case STATUS_DESPACHADO -> "Despachado";
-            case STATUS_CANCELADO -> "Cancelado";
-            default -> throw new IllegalArgumentException("Status inválido");
-        };
-
-    }
 }
--- Enumeracoes/src/Principal.java
+++ Enumeracoes/src/Principal.java
@@ -1,10 +1,8 @@
 public class Principal {
     public static void main(String[] args) {
         Pedido pedido = new Pedido();
-        pedido.setStatus(Pedido.STATUS_EMITIDO);
+        pedido.setStatus(StatusPedido.EMITIDO);
 
-        pedido.setOrigem(Pedido.ORIGEM_ONLINE);
-        System.out.println(Pedido.getDescricaoStatus(2));
-
+        pedido.setOrigem(Origem.BALCAO);
     }
 }

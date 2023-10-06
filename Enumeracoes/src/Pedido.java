2d2780d865a310999f22d48888b11bcdf946f3a0
Nesrux <joaomarcosdevs@gmail.com>
2023-10-06 12:12:03 -0300

    Enumeracoes

--- Enumeracoes/src/Pedido.java
+++ Enumeracoes/src/Pedido.java
@@ -1,2 +1,57 @@
 public class Pedido {
+    public static final int STATUS_RASCUNHO = 0;
+    public static final int STATUS_EMITIDO = 1;
+    public static final int STATUS_FATURADO = 2;
+    public static final int STATUS_DESPACHADO = 3;
+    public static final int STATUS_CANCELADO = 4;
+
+    public static final int ORIGEM_BALCAO = 100;
+    public static final int ORIGEM_ONLINE = 101;
+
+    private String nomeCliente;
+    private int status = STATUS_RASCUNHO;
+    private int origem = ORIGEM_BALCAO;
+
+
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        if (status < STATUS_RASCUNHO || status > STATUS_CANCELADO) {
+            throw new IllegalArgumentException("codigo do status inválido");
+        }
+        this.status = status;
+    }
+
+    public int getOrigem() {
+        return origem;
+    }
+
+    public void setOrigem(int origem) {
+        if (origem < ORIGEM_BALCAO || origem > ORIGEM_ONLINE) {
+            throw new IllegalArgumentException("codigo do status inválido");
+        }
+        this.origem = origem;
+    }
+
+    public static String getDescricaoStatus(int status) {
+        return switch (status) {
+            case STATUS_RASCUNHO -> "Reascunho";
+            case STATUS_FATURADO -> "Faturado";
+            case STATUS_DESPACHADO -> "Despachado";
+            case STATUS_CANCELADO -> "Cancelado";
+            default -> throw new IllegalArgumentException("Status inválido");
+        };
+
+    }
 }
--- Enumeracoes/src/Principal.java
+++ Enumeracoes/src/Principal.java
@@ -1,5 +1,10 @@
 public class Principal {
     public static void main(String[] args) {
+        Pedido pedido = new Pedido();
+        pedido.setStatus(Pedido.STATUS_EMITIDO);
+
+        pedido.setOrigem(Pedido.ORIGEM_ONLINE);
+        System.out.println(Pedido.getDescricaoStatus(2));
 
     }
 }

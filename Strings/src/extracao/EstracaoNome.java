af3ab2e82df50b33b93b0ae08c056268791f3c60
Nesrux <joaomarcosdevs@gmail.com>
2023-10-08 13:19:14 -0300

    Strings(indexof e substring)

--- Strings/src/extracao/EstracaoNome.java
+++ Strings/src/extracao/EstracaoNome.java
@@ -8,10 +8,11 @@ public class EstracaoNome {
          * caracteres depois desse indice, nesse exemplo, ele esta pegando esse indice
          * do primeiro "espaço" na nome da pessoa que no caso é o indice 5
          * e depois esta retornando toda a sequencia de caracteres posteriores*/
-        System.out.println(nome.substring(nome.indexOf(" "))); //retorno josiana vieira de sousa
 
+        System.out.println(nome.substring(nome.indexOf(" "))); //retorno josiana vieira de sousa
         //Ambos funcionam exatamente igual, porém i obtersobrenome possui uma validção
         System.out.println(obterSobrenome(nome));
+
         System.out.println(obterSegundoNome(nome));
     }
 
@@ -26,9 +27,9 @@ public class EstracaoNome {
     public static String obterSegundoNome(String nome) {
         int posicaoInicial = nome.indexOf(" ");
         int posicaoFinal = nome.indexOf(" ", posicaoInicial + 1);
-        if (posicaoInicial < 0 || posicaoFinal <= posicaoInicial) {
+        if (posicaoInicial < 0) {
             throw new RuntimeException("O nome não possui um sobrenome");
         }
-        return nome.substring(posicaoInicial, posicaoFinal);
+        return nome.substring(posicaoInicial, posicaoFinal < 0 ? nome.length() : posicaoFinal);
     }
 }

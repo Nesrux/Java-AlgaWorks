aeaff8d4239f73e4494c662c11157c519ff8d949
Nesrux <joaomarcosdevs@gmail.com>
2023-10-08 11:48:36 -0300

    Strings(validaçoes e metodos)

--- Strings/src/validacoes/strings/EqualsIgnoreCase.java
+++ Strings/src/validacoes/strings/EqualsIgnoreCase.java
@@ -1,4 +1,11 @@
 package validacoes.strings;
 
 public class EqualsIgnoreCase {
+    public static void main(String[] args) {
+        String nome = "João";
+        String nome2 = new String("JOÃO");
+        //iqualsIgnoreCase verifica se o conteudo das String são iguais
+        // ignorando se estão em maiuscula ou minuscula.
+        System.out.println(nome.equalsIgnoreCase(nome2));
+    }
 }

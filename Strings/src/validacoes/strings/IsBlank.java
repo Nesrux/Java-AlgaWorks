aec80d6ad17523b463d46f59f03e3411d13f08e0
Nesrux <joaomarcosdevs@gmail.com>
2023-10-08 11:52:21 -0300

    Strings(validaçoes e metodos)

--- Strings/src/validacoes/strings/IsBlank.java
+++ Strings/src/validacoes/strings/IsBlank.java
@@ -1,4 +1,12 @@
 package validacoes.strings;
 
 public class IsBlank {
+    public static void main(String[] args) {
+        String nome = "";
+
+        System.out.println(nome.length() == 0);
+
+        //Tem a mesma implementação que essa
+        System.out.println(nome.isEmpty());
+    }
 }
--- Strings/src/validacoes/strings/IsEmpty.java
+++ Strings/src/validacoes/strings/IsEmpty.java
@@ -1,4 +1,11 @@
 package validacoes.strings;
 
 public class IsEmpty {
+    public static void main(String[] args) {
+        String nome = "";
+        //Verifica se a String tem caracteres legiveis, ignorando
+        // o espaço
+        System.out.println(nome.isEmpty());
+
+    }
 }

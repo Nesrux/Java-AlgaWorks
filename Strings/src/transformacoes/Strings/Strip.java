49025eb9ffd48d5b47342f1256e9e5331b466264
Nesrux <joaomarcosdevs@gmail.com>
2023-10-09 16:59:52 -0300

    Strings(transformações de string)

--- Strings/src/transformacoes/Strings/Strip.java
+++ Strings/src/transformacoes/Strings/Strip.java
@@ -1,4 +1,12 @@
 package transformacoes.Strings;
 
 public class Strip {
+    public static void main(String[] args) {
+        String nome = " > " + "\n Pedro   *" + " <";
+        //Strip remove os espaços em branco dos
+        //cantos das Strings, nexessa caso o
+        // \n
+        System.out.println(nome);
+        System.out.println(nome.strip());
+    }
 }

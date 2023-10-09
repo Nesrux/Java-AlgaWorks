f8cc92b13a402e49e9b5da3ba9ad885353e8fc21
Nesrux <joaomarcosdevs@gmail.com>
2023-10-09 17:03:53 -0300

    Strings(transformações de string)

--- Strings/src/transformacoes/Strings/Replace.java
+++ Strings/src/transformacoes/Strings/Replace.java
@@ -1,4 +1,9 @@
 package transformacoes.Strings;
 
 public class Replace {
+    public static void main(String[] args) {
+        String nome = "João marcos de sousa rebelo";
+
+        System.out.println(nome.replace(" de ", " juliano s"));
+    }
 }
--- Strings/src/transformacoes/Strings/Strip.java
+++ Strings/src/transformacoes/Strings/Strip.java
@@ -2,7 +2,7 @@ package transformacoes.Strings;
 
 public class Strip {
     public static void main(String[] args) {
-        String nome = " > " + "\n Pedro   *" + " <";
+        String nome = "    > " + "  Pedro" + " <       ";
         //Strip remove os espaços em branco dos
         //cantos das Strings, nexessa caso o
         // \n

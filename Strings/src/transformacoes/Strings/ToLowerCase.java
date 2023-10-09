af8b2a99f383e680b1efd588308fb1f102138d50
Nesrux <joaomarcosdevs@gmail.com>
2023-10-09 16:57:48 -0300

    Strings(transformações de string)

--- Strings/src/transformacoes/Strings/ToLowerCase.java
+++ Strings/src/transformacoes/Strings/ToLowerCase.java
@@ -1,4 +1,11 @@
 package transformacoes.Strings;
 
 public class ToLowerCase {
+    public static void main(String[] args) {
+        String nome = "Cleiton";
+
+        String nomeMinusculo = nome.toLowerCase();
+
+        System.out.println(nomeMinusculo);
+    }
 }

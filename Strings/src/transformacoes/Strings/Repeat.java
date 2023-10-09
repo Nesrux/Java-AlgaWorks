ce1ca8fc126faca9cb623b573483b8fa33f49c21
Nesrux <joaomarcosdevs@gmail.com>
2023-10-09 17:05:11 -0300

    Strings(transformações de string)

--- Strings/src/transformacoes/Strings/Repeat.java
+++ Strings/src/transformacoes/Strings/Repeat.java
@@ -1,4 +1,8 @@
 package transformacoes.Strings;
 
 public class Repeat {
+    public static void main(String[] args) {
+        String nome = "Cleitinho XUXUCÂO ";
+        System.out.println(nome.repeat(5));
+    }
 }

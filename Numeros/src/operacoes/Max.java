b47efcc5b4a434ccad9d7d4233b22b70838109bc
Nesrux <joaomarcosdevs@gmail.com>
2023-10-13 14:24:41 -0300

    Operações (Max)

--- Numeros/src/operacoes/Max.java
+++ Numeros/src/operacoes/Max.java
@@ -1,4 +1,14 @@
 package operacoes;
 
 public class Max {
+    public static void main(String[] args) {
+        int x = 100;
+        int y = 120;
+        /*A operação Max, faz o oposto da operação Min
+         * pega 2 valores e devolve o maior deles*/
+        int z = Math.max(x, y);
+
+        System.out.println(z);
+
+    }
 }

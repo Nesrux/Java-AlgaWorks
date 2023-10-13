41cd780b874c10b9697f08bb4aeefa3b31ae7c85
Nesrux <joaomarcosdevs@gmail.com>
2023-10-13 14:23:38 -0300

    Operações (Min)

---
+++ Numeros/src/operacoes/Min.java
@@ -0,0 +1,13 @@
+package operacoes;
+
+public class Min {
+    public static void main(String[] args) {
+        int x = 100;
+        int y = 120;
+        /*A operação mim, como o nome diz é uma operação matemática que devolve
+         * o menor número de 2 variaveis, nesse caso é o 100*/
+        int z = Math.min(x, y);
+
+        System.out.println(z);
+    }
+}

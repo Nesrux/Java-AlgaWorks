ce4d02ef25d696b34c750070f8d382bd47201d3a
Nesrux <joaomarcosdevs@gmail.com>
2023-10-13 14:53:46 -0300

    Precisão com numeros (BigDecimal)

--- Numeros/src/Precisao/numeros/Principal.java
+++ Numeros/src/Precisao/numeros/Principal.java
@@ -1,4 +1,14 @@
 package Precisao.numeros;
 
+import java.math.BigDecimal;
+
 public class Principal {
+    public static void main(String[] args) {
+        BigDecimal valorTotal = new BigDecimal("2.05");
+        BigDecimal valorPago = new BigDecimal("1.05");
+
+        BigDecimal valorDevido = valorTotal.subtract(valorPago);
+
+        System.out.println(valorDevido);
+    }
 }

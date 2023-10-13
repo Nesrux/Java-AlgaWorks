8a0f14d2204aa6e3bb755bd809028866d60a0664
Nesrux <joaomarcosdevs@gmail.com>
2023-10-13 14:27:14 -0300

    Operações (Pow)

--- Numeros/src/operacoes/Pow.java
+++ Numeros/src/operacoes/Pow.java
@@ -1,4 +1,16 @@
 package operacoes;
 
 public class Pow {
+    public static void main(String[] args) {
+        double x = 100;
+        double y = 2;
+        /*A operação pow recebe como parametro 2 doubles
+         * e faz a potencia deles, primeiro valor é o numero que será
+         * elevado, e o segundo valor é o valor da pontencia*/
+        double z = Math.pow(x, y);
+
+        //10_000
+        System.out.println(z);
+
+    }
 }

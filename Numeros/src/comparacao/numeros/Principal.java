9f642230019d9db85ea92a6febd36055271509c9
Nesrux <joaomarcosdevs@gmail.com>
2023-10-13 14:20:24 -0300

    comparador de igualdade para matematica em java

---
+++ Numeros/src/comparacao/numeros/Principal.java
@@ -0,0 +1,14 @@
+package comparacao.numeros;
+
+public class Principal {
+    public static void main(String[] args) {
+        int x = 124;
+        int y = 124;
+
+        Integer a = 124;
+        Integer b = 124;
+
+        System.out.println(y == x);
+        System.out.println(a == b);
+    }
+}

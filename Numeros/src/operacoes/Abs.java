73fa428e0e9b362e9f98ca33f53db4a8c6cb7f84
Nesrux <joaomarcosdevs@gmail.com>
2023-10-13 14:31:27 -0300

    Operações (abs)

--- Numeros/src/operacoes/Abs.java
+++ Numeros/src/operacoes/Abs.java
@@ -1,4 +1,13 @@
 package operacoes;
 
 public class Abs {
+    public static void main(String[] args) {
+        float x = -100.5F;
+        float y = 100.5F;
+
+        //O método abs devolve o valor absoluto, ou seja sem
+        //o ponto flutuante e sem sinal negativo!
+        System.out.println(Math.abs(x));
+        System.out.println(Math.abs(y));
+    }
 }

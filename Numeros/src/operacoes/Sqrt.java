df1253f36633b7259414408966b4a7bf4b831a7b
Nesrux <joaomarcosdevs@gmail.com>
2023-10-13 14:28:41 -0300

    Operações (Sqrt)

--- Numeros/src/operacoes/Sqrt.java
+++ Numeros/src/operacoes/Sqrt.java
@@ -1,4 +1,12 @@
 package operacoes;
 
 public class Sqrt {
+    public static void main(String[] args) {
+        double x = 100;
+        double y = 2;
+        /*A operação sqrt, é uma operação de raiz quadrada, ela devolve o numero
+         * solicitado em sua raiz*/
+        double z = Math.sqrt(x);
+        System.out.println(z);
+    }
 }

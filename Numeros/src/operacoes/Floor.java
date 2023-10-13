7604ededa7a18049a72fd107d6219de515b6907f
Nesrux <joaomarcosdevs@gmail.com>
2023-10-13 14:34:48 -0300

    Operações (floor)

--- Numeros/src/operacoes/Floor.java
+++ Numeros/src/operacoes/Floor.java
@@ -1,4 +1,15 @@
 package operacoes;
 
 public class Floor {
+    public static void main(String[] args) {
+        double x = 100.1;
+        double y = 100.9;
+
+
+        //Ele é o inverso do ceil, ele arredonda o número para baixo
+        System.out.println(Math.floor(x));
+        System.out.println(Math.floor(y));
+
+    }
+
 }

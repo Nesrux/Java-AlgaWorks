598921b1a484ba94687895d8cb16506fdd77b416
Nesrux <joaomarcosdevs@gmail.com>
2023-10-18 16:05:30 -0300

    interfaces funcionais (Entendendo a importancia de usar as interfaces funcionais do java lang)

--- interfaces-funcionais/src/com/elementosDeColecoes/Principal.java
+++ interfaces-funcionais/src/com/elementosDeColecoes/Principal.java
@@ -2,7 +2,7 @@ package com.elementosDeColecoes;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class Principal {
     public static void main(String[] args) {
@@ -17,6 +17,12 @@ public class Principal {
         produtos.add(new Produto("1l leite integrau", new BigDecimal("3.75"), 0));
         produtos.add(new Produto("Amaciante", new BigDecimal("8.75"), 10,
                 Produto.StatusProduto.INATIVO));
+
+        Predicate<Produto> qtdaMenorQueZero = produto -> produto.getQuantidade() < 0;
+        Predicate<Produto> isInativo = produto -> Produto.StatusProduto.INATIVO.equals(produto.getStatus());
+
+        produtos.removeIf(qtdaMenorQueZero.or(isInativo));
+
 
         for (Produto produto : produtos) {
             System.out.println(produto);

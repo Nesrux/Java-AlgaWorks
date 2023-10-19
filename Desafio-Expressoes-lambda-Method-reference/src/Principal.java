15fc5ef0e6f214141566796710a48ce853a6578d
Nesrux <joaomarcosdevs@gmail.com>
2023-10-19 11:18:54 -0300

    Desafio expressoes lambda e method reference (Completo)

--- Desafio-Expressoes-lambda-Method-reference/src/Principal.java
+++ Desafio-Expressoes-lambda-Method-reference/src/Principal.java
@@ -1,24 +1,32 @@
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Principal {
 
     public static void main(String[] args) {
-		var funcionarios = new ArrayList<Funcionario>();
-		funcionarios.add(new Funcionario("João", new BigDecimal("19000"), true));
-		funcionarios.add(new Funcionario("Maria", new BigDecimal("5000"), true));
-		funcionarios.add(new Funcionario("Manoel", new BigDecimal("13000"), true));
-		funcionarios.add(new Funcionario("Sebastião", new BigDecimal("12000"), false));
+        var funcionarios = new ArrayList<Funcionario>();
+        funcionarios.add(new Funcionario("João", new BigDecimal("19000"), true));
+        funcionarios.add(new Funcionario("Maria", new BigDecimal("5000"), true));
+        funcionarios.add(new Funcionario("Manoel", new BigDecimal("13000"), true));
+        funcionarios.add(new Funcionario("Sebastião", new BigDecimal("12000"), false));
 
-		// TODO remover funcionários inativos
+        // TODO remover funcionários
+        funcionarios.removeIf(Funcionario::isInativo);
 
-		// TODO ordenar funcionários pelo valor do salário
+        // TODO ordenar funcionários pelo valor do salário
+        funcionarios.sort(Comparator.comparing(Funcionario::getSalario));
 
-		// TODO iterar e imprimir funcionários usando o método estático imprimir
-	}
+        // TODO iterar e imprimir funcionários usando o método estático imprimir
+        funcionarios.forEach(Principal::imprimir);
+    }
 
-	private static void imprimir(Funcionario funcionario) {
-		// TODO implementar a impressão do nome, salário e impostos (20%)
-	}
+    private static void imprimir(Funcionario funcionario) {
+        // TODO implementar a impressão do nome, salário e impostos (20%)
+        BigDecimal descontoSalario = funcionario.getSalario().multiply(new BigDecimal("0.20"));
+        System.out.printf("O funcionário %s tem o salario de %.2f e os descontos são %.2f%n", funcionario.getNome(),
+                funcionario.getSalario(), descontoSalario);
+
+    }
 
 }

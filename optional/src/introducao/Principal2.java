5edf96edf9ce885f168473168656c110520f31bb
Nesrux <joaomarcosdevs@gmail.com>
2023-10-23 17:31:56 -0300

    Classe optional (Map)

--- optional/src/introducao/Principal2.java
+++ optional/src/introducao/Principal2.java
@@ -13,31 +13,40 @@ public class Principal2 {
 
         servicoBagagens.contratar("2ABCS59Y", 11);
         //
-        servicoReserva.buscar("asdfds")
-                .orElse(null);
+//        servicoReserva.buscar("asdfds")
+//                .orElse(null);
+//
+//        /*OrElseGet devolve uma instancia diferente do selecionado.*/
+//        servicoReserva.buscar("456")
+//                .orElseGet(() -> new Reserva("455", voo, "gerundina"));
+//
+//        /*IfPresent e IfpresentOrelse, são metodos autoexplicativos que utilizam os metodos
+//         * isPresent e isEmpity da classe Optional, para fazer um If*/
+//        servicoReserva.buscar("2ACFTS59")
+//                .ifPresent(reserva -> reserva.adicionarBagagens(10));
+//
+//
+//        servicoReserva.buscar("2AB8859W")
+//                .ifPresentOrElse(reserva -> reserva.adicionarBagagens(10),
+//                        () -> System.out.println("Não presente :/"));
+//        //   servicoReserva.getReservas().forEach(System.out::println);
+//
+//
+//        /*Filter é explicativo, ele vai filtrar o optional e verificar usando um predicate
+//         * se a condição é valida, se sim ele nao executa o orelsetrhrow*/
+//        Reserva reserva = servicoReserva.buscar("2ABCS59Y")
+//                .filter(reserva1 -> reserva1.getQuantidadeBagagens() > 10)
+//                .orElseThrow(RuntimeException::new);
+//
+//
+        /*O Map, serve para pegar variaveis de intancias ou utilizar métodos dentro da intancia
+         * sem precisar atribuir uma nova variavel para executar esse método */
 
-        /*OrElseGet devolve uma instancia diferente do selecionado.*/
-        servicoReserva.buscar("456")
-                .orElseGet(() -> new Reserva("455", voo, "gerundina"));
-
-        /*IfPresent e IfpresentOrelse, são metodos autoexplicativos que utilizam os metodos
-         * isPresent e isEmpity da classe Optional, para fazer um If*/
-        servicoReserva.buscar("2ACFTS59")
-                .ifPresent(reserva -> reserva.adicionarBagagens(10));
-
-
-        servicoReserva.buscar("2AB8859W")
-                .ifPresentOrElse(reserva -> reserva.adicionarBagagens(10),
-                        () -> System.out.println("Não presente :/"));
-        //   servicoReserva.getReservas().forEach(System.out::println);
-
-
-        /*Filter é explicativo, ele vai filtrar o optional e verificar usando um predicate
-         * se a condição é valida, se sim ele nao executa o orelsetrhrow*/
-        Reserva reserva = servicoReserva.buscar("2ABCS59Y")
-                .filter(reserva1 -> reserva1.getQuantidadeBagagens() > 10)
+        Passageiro passageiro = servicoReserva.buscar("2ABCS59Y")
+                .filter(r -> r.getQuantidadeBagagens() > 10)
+                .map(Reserva::getPassageiro)
                 .orElseThrow(RuntimeException::new);
 
-        System.out.println(reserva);
+        System.out.println(passageiro);
     }
 }

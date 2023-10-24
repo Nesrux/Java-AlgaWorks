a14e73a4848c6b75eff806b677c80a3a16da166d
Nesrux <joaomarcosdevs@gmail.com>
2023-10-24 17:22:38 -0300

    Stream api (introdução)

--- Streams/src/inicio/streams/Principal.java
+++ Streams/src/inicio/streams/Principal.java
@@ -1,4 +1,25 @@
 package inicio.streams;
 
+import inicio.streams.estoque.CadastroProduto;
+import inicio.streams.estoque.Produto;
+
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Principal {
+    public static void main(String[] args) {
+        CadastroProduto cadastroProduto = new CadastroProduto();
+        List<Produto> produtos = cadastroProduto.obterTodos();
+
+        /*Isso se chama pipeline, pipeline é uma sequencia de métodos utilizado
+         * para criar ou modificar algo, nesse caso, filtrando as Streams*/
+        Stream<Produto> stream = produtos
+                .stream();
+        /**/
+        Stream<Produto> produtoStream = stream
+                .filter(Produto::temEstoque);
+
+        Stream<Produto> streamcomEstoqueIniativo = produtoStream.
+                filter(Produto::isInativo);
+    }
 }

29f033c2bd2a471e422ea4023cbbd8436d84ae6a
Nesrux <joaomarcosdevs@gmail.com>
2023-10-24 18:34:17 -0300

    Stream api (peek)

--- Streams/src/inicio/streams/principal2.java
+++ Streams/src/inicio/streams/principal2.java
@@ -12,12 +12,16 @@ public class principal2 {
         /*Esse código faz exatamente a nesna coisa que a classe Principal
          * só que de uma forma mais enchuta*/
 
+        /*O método peek do ingles (olhadinha), é utilizado principalmente para fazer debug
+         * porém, também pode ser utilizado para ou executar um método durante o a pipeline
+         * a*/
         produtos.stream()
+                .peek(produto -> produto.setNome(produto.getNome().toUpperCase()))
                 .filter(Produto::temEstoque)
                 .filter(Produto::isInativo)
                 .forEach(produto -> {
                     produto.ativar();
-                    System.out.println(produto);
+                    System.out.println(produto.getNome());
                 });
     }
 }

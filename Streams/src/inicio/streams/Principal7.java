17a7948f6a7319744576499e690edecb8e1ead2e
Nesrux <joaomarcosdevs@gmail.com>
2023-10-25 19:27:32 -0300

    Stream api (distinct)

--- Streams/src/inicio/streams/Principal7.java
+++ Streams/src/inicio/streams/Principal7.java
@@ -16,9 +16,15 @@ public class Principal7 {
          * acessar esse fabricante, transformando uma stream de produtos
          * em uma stream de fabricantes, ele nao altera os elementos da stream
          * só deixa eu acessalos para fazer quaisquer alterações*/
+
+        /*distinct é um método intermediario que usa o método equals
+         * ele faz que nem o distinct do SQL, ele retorna a stream com
+         * distinção de tipo, ou objetos iguais nao podem acontecer.*/
+        //Ela é uma operação statefull
         produtos.stream()
                 .filter(Produto::temEstoque)
                 .map(Produto::getFabricante)
+                .distinct()
                 .forEach(fabricante -> System.out.println(fabricante.nome()));
     }
 }

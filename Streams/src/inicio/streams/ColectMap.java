99c11e05fa67a9a53e504a34cddc5b7be2e1687d
Nesrux <joaomarcosdevs@gmail.com>
2023-10-27 18:38:34 -0300

    Stream api (mapas agrupados com o groupby)

--- Streams/src/inicio/streams/ColectMap.java
+++ Streams/src/inicio/streams/ColectMap.java
@@ -1,6 +1,7 @@
 package inicio.streams;
 
 import inicio.streams.estoque.CadastroProduto;
+import inicio.streams.estoque.Fabricante;
 import inicio.streams.estoque.Produto;
 
 import java.util.List;
@@ -16,5 +17,15 @@ public class ColectMap {
                 .filter(Produto::temEstoque)
                 .collect(Collectors.toMap(Produto::getNome, Produto::getQuantidade));
         System.out.println(estoque);
+
+        Map<Fabricante, List<Produto>> produtosFabricante = produtos.stream()
+                .filter(Produto::temEstoque)
+                .collect(Collectors.groupingBy(Produto::getFabricante));
+        produtosFabricante.forEach((fabricante, lista) ->{
+            System.out.println();
+            System.out.println(fabricante.nome());
+            System.out.println("--------");
+            lista.forEach(produto -> System.out.println(produto.getNome()));
+        });
     }
 }

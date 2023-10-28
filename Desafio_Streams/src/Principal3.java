c12366d1c40e8cd9003c08f7b54cc003b218a554
Nesrux <joaomarcosdevs@gmail.com>
2023-10-28 10:49:12 -0300

    Desafio Stream api (3) completo

--- Desafio_Streams/src/Principal3.java
+++ Desafio_Streams/src/Principal3.java
@@ -1,8 +1,7 @@
 import com.cormercial.ServicoDeVenda;
 import com.cormercial.Venda;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Principal3 {
@@ -11,22 +10,32 @@ public class Principal3 {
         var servicoDeVenda = new ServicoDeVenda();
         List<Venda> vendas = servicoDeVenda.obterTodas();
 
-        List<String> descricoes = new ArrayList<>();
-        for (Venda venda : vendas) {
-            if (venda.isFechada()) {
-                for (Venda.Item item : venda.getItens()) {
-                    if (!descricoes.contains(item.descricao())) {
-                        descricoes.add(item.descricao());
-                    }
-                }
-            }
-        }
+        vendas.stream()
+                .filter(Venda::isFechada)
+                .flatMap(venda -> venda.getItens().stream())
+                .map(Venda.Item::descricao)
+                .distinct()
+                .sorted()
+                .forEach(System.out::println);
 
-        Collections.sort(descricoes);
 
-        for (String descricao : descricoes) {
-            System.out.println(descricao);
-        }
+//
+//        List<String> descricoes = new ArrayList<>();
+//        for (Venda venda : vendas) {
+//            if (venda.isFechada()) {
+//                for (Venda.Item item : venda.getItens()) {
+//                    if (!descricoes.contains(item.descricao())) {
+//                        descricoes.add(item.descricao());
+//                    }
+//                }
+//            }
+//        }
+//
+//        Collections.sort(descricoes);
+//
+//        for (String descricao : descricoes) {
+//            System.out.println(descricao);
+//        }
     }
 
 }

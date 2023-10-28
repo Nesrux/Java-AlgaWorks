a7e7e8dc308a7e3962ca5100ed2c24324c53d828
Nesrux <joaomarcosdevs@gmail.com>
2023-10-28 10:25:00 -0300

    Desafio Stream api (1) completo

--- Desafio_Streams/src/Principal1.java
+++ Desafio_Streams/src/Principal1.java
@@ -9,12 +9,10 @@ public class Principal1 {
     public static void main(String[] args) {
         var servicoDeVenda = new ServicoDeVenda();
         List<Venda> vendas = servicoDeVenda.obterTodas();
+        vendas.stream()
+                .filter(Venda::isFechada)
+                .forEach(System.out::println);
 
-        for (Venda venda : vendas) {
-            if (venda.isFechada()) {
-                System.out.println(venda);
-            }
-        }
     }
 
 }

2777cff34a9a64a79602229281f949c99ab8e64e
Nesrux <joaomarcosdevs@gmail.com>
2023-10-28 11:12:40 -0300

    Desafio Stream api (6) completo

--- Desafio_Streams/src/Principal6.java
+++ Desafio_Streams/src/Principal6.java
@@ -1,29 +1,35 @@
-
-
+import com.cormercial.Cliente;
 import com.cormercial.ServicoDeVenda;
 import com.cormercial.Venda;
 
-import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Principal6 {
 
     public static void main(String[] args) {
         var servicoDeVenda = new ServicoDeVenda();
         List<Venda> vendas = servicoDeVenda.obterTodas();
+        List<Cliente> clientes = vendas.stream()
+                .filter(Venda::isFechada)
+                .map(Venda::getCliente)
+                .distinct()
+                .sorted(Comparator.comparing(Cliente::nome))
+                .toList();
 
-        List<String> clientes = new ArrayList<>();
+//        List<String> clientes = new ArrayList<>();
+//
+//        for (Venda venda : vendas) {
+//            if (venda.isFechada()) {
+//                if (!clientes.contains(venda.getCliente().nome())) {
+//                    clientes.add(venda.getCliente().nome());
+//                }
+//            }
+//        }
 
-        for (Venda venda : vendas) {
-            if (venda.isFechada()) {
-                if (!clientes.contains(venda.getCliente().nome())) {
-                    clientes.add(venda.getCliente().nome());
-                }
-            }
-        }
-
-        Collections.sort(clientes);
+//      Collections.sort(clientes);
         System.out.println(clientes);
     }
 

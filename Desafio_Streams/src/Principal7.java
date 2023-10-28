738bb989775eaf402e6a7d9c80fc5032a94ad9c0
Nesrux <joaomarcosdevs@gmail.com>
2023-10-28 11:33:35 -0300

    Desafio streams completo

--- Desafio_Streams/src/Principal7.java
+++ Desafio_Streams/src/Principal7.java
@@ -5,6 +5,7 @@ import com.cormercial.Venda;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Principal7 {
 
@@ -12,19 +13,25 @@ public class Principal7 {
         var servicoDeVenda = new ServicoDeVenda();
         List<Venda> vendas = servicoDeVenda.obterTodas();
 
-        Map<String, Long> vendasRealizadasPorCliente = new HashMap<>();
+      //  Map<String, Long> vendasRealizadasPorCliente = new HashMap<>();
 
-        for (Venda venda : vendas) {
-            if (venda.isFechada()) {
-                String nomeCliente = venda.getCliente().nome();
-                Long quantidadeVendas = vendasRealizadasPorCliente.get(nomeCliente);
-                if (quantidadeVendas == null) {
-                    quantidadeVendas = 0L;
-                }
+        Map<String, Long> vendasRealizadasPorCliente = vendas.stream()
+                .filter(Venda::isFechada)
+                .collect(Collectors.groupingBy(venda -> venda.getCliente().nome(),
+                        Collectors.counting()));
 
-                vendasRealizadasPorCliente.put(nomeCliente, quantidadeVendas + 1);
-            }
-        }
+//
+//        for (Venda venda : vendas) {
+//            if (venda.isFechada()) {
+//                String nomeCliente = venda.getCliente().nome();
+//                Long quantidadeVendas = vendasRealizadasPorCliente.get(nomeCliente);
+//                if (quantidadeVendas == null) {
+//                    quantidadeVendas = 0L;
+//                }
+//
+//                vendasRealizadasPorCliente.put(nomeCliente, quantidadeVendas + 1);
+//            }
+//        }
 
         System.out.println(vendasRealizadasPorCliente);
     }

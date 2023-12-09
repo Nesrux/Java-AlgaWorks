156a765c868473a40bc006d6e04d4eaaccb4ca27
Nesrux <joaomarcosdevs@gmail.com>
2023-12-09 13:49:27 -0300

    Feat: refatorando para utilizar Linkedlist

--- collections_api/src/linkedList/Principal.java
+++ collections_api/src/linkedList/Principal.java
@@ -17,6 +17,17 @@ public class Principal {
         hoteis.addLast(new Hotel("elma maria hostel", "chabrinquei", 24));
 
         imprimirHoteis(cadastro.obterTodos());
+
+        /*Anotações do Jaaum*/
+        /*LinkedList, que no caso do Java, é um Double Linked List do inglês,
+        * lista duplamente ligada, é uma implementação da interface List, que visa
+        * performance, da remoção e asserção de valores dentro da lista, ela nao utiliza
+        * internamente arrays, ela utilza de indices para fazer a iteração da lista, então
+        * se a linkedList for muito grande vai ter graves problemas de performance, pois
+        * sempre sera preciso iterar toda a lista para achar um valor esspecifico, porém
+        * na Hora de adicionar ou remover objetos é muito mais performaticos, pois como não
+        * utiliza de arrays, não precisa ficar dupplicando todx para aumentar o tamnho,
+        * ganhando muita performance do processo*/
     }
 
     private static void imprimirHoteis(LinkedList<Hotel> hoteis) {

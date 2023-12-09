16627c2d1629791e56482dea381e827fc0d73da8
Nesrux <joaomarcosdevs@gmail.com>
2023-12-09 19:24:40 -0300

    adicionando métodos de ordenação

---
+++ collections_api/src/arrayList/OrdenacaoDeListas.java
@@ -0,0 +1,33 @@
+package arrayList;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class OrdenacaoDeListas {
+    public static void main(String[] args) {
+        List<Integer> numeros = new ArrayList<>();
+        numeros.add(10);
+        numeros.add(55);
+        numeros.add(12);
+        numeros.add(982);
+        numeros.add(25);
+        numeros.add(1);
+        numeros.add(5);
+        //       numeros.sort( Integer::compareTo);
+        // Collections.sort(numeros);
+        Collections.sort(numeros, Comparator.reverseOrder());
+        List<String> letras = new ArrayList<>();
+
+        letras.add("a");
+        letras.add("z");
+        letras.add("w");
+        letras.add("t");
+        letras.add("h");
+        letras.sort(String::compareTo);
+
+        System.out.println(numeros);
+        System.out.println(letras);
+    }
+}
